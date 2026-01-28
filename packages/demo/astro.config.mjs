3e6f9198fbe4dbf88384a23277c0786fc3ecd611
Alex Polvi <alex@polvi.net>
2026-01-27 17:42:10 -0800

    remove server

--- packages/demo/astro.config.mjs
+++ packages/demo/astro.config.mjs
@@ -5,7 +5,6 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
     integrations: [preact()],
-    output: 'server',
     adapter: cloudflare(),
     image: {
         service: {
